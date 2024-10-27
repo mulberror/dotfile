0b588640fd9c6b611d53334692563abf3eea9621
mulberror <mulberror@gmail.com>
2024-10-27 16:37:23 +0800

    add: vscode background

--- vscode/glazing.js
+++
@@ -1,5 +0,0 @@
-nodeRequire('electron')
-    .remote
-    .getCurrentWindow()
-    // 设置半透明效果  https://electronjs.org/docs/api/browser-window#winsetvibrancytype-macos 
-    .setVibrancy('ultra-dark');
